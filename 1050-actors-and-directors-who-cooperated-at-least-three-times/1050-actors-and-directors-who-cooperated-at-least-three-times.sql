b6ea05bdbe77d1970724f8d28641d2f2980662a4
U HYUN <121507181+yu-hyun2@users.noreply.github.com>
2023-03-20 16:01:18 +0900

    Time: 1107 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,7 +1,3 @@
-# Write your MySQL query statement below
-# 배우가 감독과 세 번 이상 협력한 쌍(배우_id, 감독_id)을 제공하는 보고서에 대한 SQL 쿼리를 작성합니다.
-# 결과 테이블을 임의의 순서로 반환합니다.
-
 SELECT actor_id, director_id
 FROM ActorDirector
 GROUP BY actor_id, director_id
